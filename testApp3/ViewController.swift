992c62803576d65b9ba68978909861cb4e8419d7
Artem Paklinov <apaklinov@yandex.ru>
2019-12-20 21:12:58 +0700

    The flashlight is working

--- testApp3/ViewController.swift
+++ testApp3/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Artem Paklinov on 18.12.2019.
 //  Copyright © 2019 Artem Paklinov. All rights reserved.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,15 +14,23 @@ class ViewController: UIViewController {
     
     @IBAction func pushOnLight(_ sender: UIButton) {
         ligthOn = !ligthOn
-        updateUI()
+        updateView()
 
     }
     
-    func updateUI() {
-        if ligthOn {
-            view.backgroundColor = UIColor.white
-        } else {
+    func updateView() {
+        let device = AVCaptureDevice.default(for: .video)
+        if let dev = device, dev.hasTorch {
             view.backgroundColor = UIColor.black
+            do {
+                try dev.lockForConfiguration()
+                dev.torchMode = ligthOn ? .on : .off
+                dev.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        } else {
+            view.backgroundColor = ligthOn ? .white : .black
         }
     }
     
@@ -38,6 +46,11 @@ class ViewController: UIViewController {
     @IBAction func upButton(_ sender: UIButton) {
         myLabel.text = "Кнопка не нажата"
     }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
